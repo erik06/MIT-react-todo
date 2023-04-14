2222405cf03cf7d955efa528ed3e4400eb3a4a5c
Erik Slagter <erikslagter@gmail.com>
2023-04-13 21:58:56 -0500

    updated project root

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://erik06.github.io/MIT-react-todo/",
   root: "dist",
   build: {
     outDir: "../docs",
